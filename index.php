b46616854e034bbc23e8630ebda7f5ee9505a2c9
kigor1998 <89158409890@yandex.ru>
2020-11-18 21:02:30 +0300

    вёрстка

--- index.php
+++
@@ -1,8 +0,0 @@
-<?php
- $is_string = " ' " . " ' " . " ' ";
- $is_numer = 228;
- $is_bool = 0;
- $is_array = [228, 0, 'dasdas'];
-$is_porn = "Олег хороший мальчик";
-$is_rage = "Олег"."хороший"."мальчик";
-$dhfjdsk = 'fsdfsd';
